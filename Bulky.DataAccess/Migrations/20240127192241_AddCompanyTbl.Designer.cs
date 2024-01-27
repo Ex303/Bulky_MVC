51f4201f89bab82c1043664314d84af1c06e70ac
NOAH <jhonrsese@gmail.com>
2024-01-28 04:05:43 +0800

    127. Display Company Dropdown

--- Bulky.DataAccess/Migrations/20240127192241_AddCompanyTbl.Designer.cs
+++ Bulky.DataAccess/Migrations/20240127192241_AddCompanyTbl.Designer.cs
@@ -4,6 +4,7 @@ using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bulky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240127192241_AddCompanyTbl")]
+    partial class AddCompanyTbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -79,9 +82,8 @@ namespace Bulky.DataAccess.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("PhoneNumber")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<int>("PhoneNumber")
+                        .HasColumnType("int");
 
                     b.Property<string>("PostalCode")
                         .IsRequired()
---
+++ BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+	{
+
+		private readonly IUnitOfWork _UnitOfWork;
+		private readonly IWebHostEnvironment _WebHostEnvironment;
+		public CompanyController(IUnitOfWork unitOfWork)
+		{
+			_UnitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			List<Company> objCompanyList = _UnitOfWork.Company.GetAll().ToList();
+			
+			return View(objCompanyList);
+		}
+
+		public IActionResult Upsert(int? id)
+		{
+
+
+			if (id == null || id == 0)
+				return View(new Company());
+			else
+			{
+				Company obj = _UnitOfWork.Company.Get(u => u.Id == id);
+				return View(obj);
+			}
+				
+		}
+		[HttpPost]
+		public IActionResult Upsert(Company ojb)
+		{
+			//if (obj.Name == obj.DisplayOrder.ToString())
+			//{
+			//    ModelState.AddModelError("Name","The Display Order cannot exaclty match the Name.");
+			//}
+
+			if (ModelState.IsValid)
+			{ 
+				if(ojb.Id == 0)
+					_UnitOfWork.Company.Add(ojb);
+				else
+					_UnitOfWork.Company.Update(ojb);
+
+
+				_UnitOfWork.Save();
+				TempData["success"] = "Company created successfully";
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View(ojb);
+			}
+
+		}
+
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+            List<Company> objCompanyList = _UnitOfWork.Company.GetAll().ToList();
+			return Json(new {data = objCompanyList });
+        }
+		[HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+			var CompanyToBeDeleted = _UnitOfWork.Company.Get(u => u.Id == id);
+			if (CompanyToBeDeleted == null)
+			{
+				return Json(new { success = false, message = "Error while deleting" });
+			}
+
+			_UnitOfWork.Company.Remove(CompanyToBeDeleted);
+			_UnitOfWork.Save();
+
+            List<Company> objCompanyList = _UnitOfWork.Company.GetAll().ToList();
+            return Json(new { success = true, message = "Delete Succesful" });
+        }
+        #endregion
+
+    }
+}
